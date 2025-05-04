c726bdc53a5e89065749854b879ae7fe5be665a9
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-05-03 22:13:40 -0600

    image blur kernel written
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

---
+++ day3/imageBlur.cu
@@ -0,0 +1,22 @@
+__global__
+void imageBlur(unsigned char *in, unsigned char *out, int width, int height) {
+    int col = threadIdx.x + blockIdx.x * blockDim.x;
+    int row = threadIdx.y + blockIdx.y * blockDim.y;
+    if (row < height && col < width) {
+        int pixVal = 0;
+        int pixels = 0;
+        for (int i = -1; i <= 1; i++) { // 1 is the BLUR_SIZE
+            for (int j = -1; j <= 1; j++) {
+                int curRow = row + i;
+                int curCol = col + j;
+                if (curRow >= 0 && curRow < height && curCol >= 0 && curCol < width) {
+                    pixels++;
+                    pixVal += in[curRow * width + curCol];
+                    
+                }
+            }
+        }
+
+        out[row * width + col] = (unsigned char)(pixVal / pixels);
+    }
+}
