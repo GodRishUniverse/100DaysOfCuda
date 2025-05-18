b3178634d3d80df57c4f7ee303989fd2a85ee41c
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-05-18 16:49:11 -0600

    better understood tiled mat mul
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- day7/README.md
+++ day7/README.md
@@ -8,6 +8,8 @@ Tiled matrix Multiplication
 
     - The basic idea to load into shared memory is to reduce the number of accesses to the gloabl memory which is slower. Reduction is by the width of the tile if the tile is`WIDTH x WIDTH`
 
-    -
+    - The first`__syncthreads()` in tiles mat mul is called "red-after-write depenedence" - because threads must waut for the data to be read by all threads that need it before overwiting it. Other name is `true dependence`
 
-    -
+    - The second`__syncthreads()` is called "write-after-read dependence" because a thread must wait for the data to be read by all threads that need it beofre overwriting it. Other name is `false dependence`
+
+    - A read-after-write dependence is a true dependence because the reading thread truly needs the data supplied  by the writing thread, so it has no choice but to wait for it. A write-after-read dependence is a false dependence because the writing thread does not need any data from the reading thread. The dependence is caused by the fact that they are reusing the same memory location and would not exist if they used different locations.
--- day7/tiled_matmul.cu
+++ day7/tiled_matmul.cu
@@ -1,28 +1,36 @@
 #define  TILE_WIDTH 16
 __global__
 void tiled_matmul_kernel(float* M, float*N, float *P, int Width){
-    __shared__ float Mds[TILE_WIDTH][TILE_WIDTH];
-    __shared__ float Nds[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float Mds[TILE_WIDTH][TILE_WIDTH]; // scope of shared memory is a block
+    __shared__ float Nds[TILE_WIDTH][TILE_WIDTH]; // scope of shared memory is a block
 
+    // one version for Mds and Nds for each block 
+
+    // shorter name auto variables - placed in registers
     int bx = blockIdx.x; 
     int by = blockIdx.y;
 
     int tx = threadIdx.x;
     int ty = threadIdx.y;
 
-    int Row = by * TILE_WIDTH+ ty;
-    int Col = bx * TILE_WIDTH +tx;
+    // each thread is responsible for calculating one value of P element
 
-    float Pvalue {};
-    for (int p_h = 0; p_h < Width/ TILE_WIDTH; ++p_h){
+    int Row = by * TILE_WIDTH+ ty; // vertical position - same analogy is below for the vertical position 
+    int Col = bx * TILE_WIDTH +tx; // horizontal position - bx blocks of threads (bx * TILE_WIDTH) before tx (thread)
+
+    float Pvalue {}; // automatic variable - so generated per 
+
+    for (int p_h = 0; p_h < Width/ TILE_WIDTH; ++p_h){ // p_h is going tile by tile
         Mds[ty][tx] = M[Row*Width +p_h*TILE_WIDTH+tx];
         Nds[ty][tx] = N[(p_h*TILE_WIDTH+ty)*Width +Col];
-        __syncthreads();
+        __syncthreads(); // used to sync the threads to do this in parallel - IN A BLOCK
+    
 
+        // This is the dot product of the row in M with the column in N
         for (int k = 0;k <TILE_WIDTH; ++k ){
             Pvalue +=Mds[ty][k] * Nds[k][tx];
         }
-        __syncthreads();
+        __syncthreads(); // acts as a barrier to ensure the threads are all synchorinized at this point
 
     }
 
